9d85b3214fe3be1930aaae6b02877ae57f46ba0e
Phil Schatzmann <phil.schatzmann@gmail.com>
2020-12-11 15:04:23 +0000

    pin support

---
+++ .ipynb_checkpoints/Readme-checkpoint.md
@@ -0,0 +1,19 @@
+# An Arduino Emulator (for Jupyter)
+
+I really wanted to have an interactive [Jupyter](https://jupyter.org/) environemnt in which I could play around interactivly with [Arduino](https://www.arduino.cc/) commands and when I discovered that Arduino provides a good starting point with their [ArduinoCore-API](https://github.com/arduino/ArduinoCore-API/tree/105276f8d81413391b14a3dc6c80180ee9e33d56) I started to draft this prototype.
+
+I am using [xeus-cling](https://github.com/jupyter-xeus/xeus-cling) as a runtime environment to simulate an Arduino Development board and I have added the related implementation using C or the C++ std library.
+
+The goal is finally to be able to provide different alternative implementations for accessing the pins
+- Using wiringPI to be run on a Raspberry PI
+- Output to a file from where it can be analysed
+- Using a Stream interface to communicate with an real Arduino Device (e.g. via udp)
+- Using some standard Linux userspace APIs for GPIO
+
+I have a __first working prototype__ ready but naturally there is still a lot to do:
+
+## Content
+- [Setup](01-Setup.ipynb)
+- [Introduction into some Basic Commands](02-BasicCommands.ipynb)
+- [The Network Stack](03-Network.ipynb)
+- [Using Pins](04-Pins.ipynb)
---
+++ ArduinoCore-Linux/cores/arduino/.ipynb_checkpoints/HardwareI2CEx-checkpoint.cpp
@@ -0,0 +1,56 @@
+#include "Hardware.h"
+#include "HardwareI2C.h"
+#include "Service.h"
+/**
+ * We suppport different implementations for the I2C
+ **/
+
+namespace arduino {
+
+
+void HardwareI2C::begin() {
+    Hardware.i2c->begin();
+}
+    
+void HardwareI2C::begin(uint8_t address) {
+    Hardware.i2c->begin(address);
+}
+    
+void HardwareI2C::end(){
+    Hardware.i2c->end();
+}
+
+void HardwareI2C::setClock(uint32_t freq){
+    Hardware.i2c->setClock(freq);
+}
+
+void HardwareI2C::beginTransmission(uint8_t address){
+    Hardware.i2c->beginTransmission(address);
+}
+    
+uint8_t HardwareI2C::endTransmission(bool stopBit){
+    return Hardware.i2c->endTransmission(stopBit);
+}
+    
+uint8_t HardwareI2C::endTransmission(void){
+    return Hardware.i2c->endTransmission();
+}
+
+uint8_t HardwareI2C::requestFrom(uint8_t address, size_t len, bool stopBit){
+    return Hardware.i2c->requestFrom(address,len,stopBit);
+
+}
+    
+uint8_t HardwareI2C::requestFrom(uint8_t address, size_t len){
+    return Hardware.i2c->requestFrom(address,len);
+}
+
+//void HardwareI2C::onReceive(void(*)(int)onReceiveCB){
+//    Hardware.i2c->onReceive(onReceiveCB);
+//}
+    
+//void HardwareI2C::onRequest(void(*)(void)onRequestCB){
+//    Hardware.i2c->onRequest(onRequestCB);
+//}
+
+}
