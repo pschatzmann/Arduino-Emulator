ddada8ada886bb7c4adf9448ba5fe41cc244f36c
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-10-28 18:34:57 +0200

    Wifi.h

--- ArduinoCore-Linux/cores/arduino/WiFi.h
+++ ArduinoCore-Linux/cores/arduino/WiFi.h
@@ -5,48 +5,51 @@
 
 /**
  * @brief We provide the WiFi class to simulate the Arduino WIFI. In in Linux
- * we can expect that networking is already active. So we dont need to do anything
- * 
+ * we can expect that networking is already active. So we dont need to do
+ * anything
+ *
  */
 
 namespace arduino {
 
 typedef enum {
-	WL_NO_SHIELD = 255,
-        WL_IDLE_STATUS = 0,
-        WL_NO_SSID_AVAIL,
-        WL_SCAN_COMPLETED,
-        WL_CONNECTED,
-        WL_CONNECT_FAILED,
-        WL_CONNECTION_LOST,
-        WL_DISCONNECTED
+  WL_NO_SHIELD = 255,
+  WL_IDLE_STATUS = 0,
+  WL_NO_SSID_AVAIL,
+  WL_SCAN_COMPLETED,
+  WL_CONNECTED,
+  WL_CONNECT_FAILED,
+  WL_CONNECTION_LOST,
+  WL_DISCONNECTED
 } wl_status_t;
-    
-    
-class WifiMock {
-  public:
-    virtual void begin(const char*name, const char* pwd){
-        // nothing to be done
-    }
-    
-    // we assume the network to be available on a desktop or host machine
-    wl_status_t status() {
-        return WL_CONNECTED;
-    }
-    
-    IPAddress &localIP(){
-        SocketImpl sock;
-        adress.fromString(sock.getIPAddress());
-        return adress;
-    }
-    
-  protected:
-    IPAddress adress;
-    
 
+class WifiMock {
+public:
+  virtual void begin(const char *name, const char *pwd) {
+    // nothing to be done
+  }
+
+  // we assume the network to be available on a desktop or host machine
+  wl_status_t status() { return WL_CONNECTED; }
+
+  IPAddress &localIP() {
+    SocketImpl sock;
+    adress.fromString(sock.getIPAddress());
+    return adress;
+  }
+
+  void setSleep(bool){
+  }
+
+  int macAddress() {
+    return mac;
+  }
+
+protected:
+  IPAddress adress;
+  int mac = 0;
 };
-    
+
 extern WifiMock WiFi;
 
-}
-
+} // namespace arduino
