b511b211c35390a7fad18aef881bfb34e341c5e9
Serge Ageyev <justboss777@gmail.com>
2025-01-30 23:32:20 +0200

    Added guard code to support empty implementation of GPIO provider

--- ArduinoCore-Linux/cores/arduino/HardwareGPIO.cpp
+++ ArduinoCore-Linux/cores/arduino/HardwareGPIO.cpp
@@ -11,50 +11,76 @@
 namespace arduino {
 
 void pinMode(pin_size_t pinNumber, PinMode pinMode){
-    Hardware.gpio->pinMode(pinNumber,pinMode);
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->pinMode(pinNumber,pinMode);
+    }
 }
 
 void digitalWrite(pin_size_t pinNumber, PinStatus status){
-    Hardware.gpio->digitalWrite(pinNumber, status);
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->digitalWrite(pinNumber, status);
+    }
 }
 
 PinStatus digitalRead(pin_size_t pinNumber){
-    return Hardware.gpio->digitalRead(pinNumber);
+    if (Hardware.gpio != nullptr) {
+        return Hardware.gpio->digitalRead(pinNumber);
+    } else {
+        return HIGH; // return high, simulate weak pullup
+    }
 }
 
 int analogRead(pin_size_t pinNumber){
-    return Hardware.gpio->analogRead(pinNumber);
+    if (Hardware.gpio != nullptr) {
+        return Hardware.gpio->analogRead(pinNumber);
+    } else {
+        return 0;
+    }
 }
 
 void analogReference(uint8_t mode){
-    Hardware.gpio->analogReference(mode);
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->analogReference(mode);
+    }
 }
 
 void analogWrite(pin_size_t pinNumber, int value){
-    Hardware.gpio->analogWrite(pinNumber, value);
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->analogWrite(pinNumber, value);
+    }
 }
     
 
 // Generates a square wave of the specified frequency (and 50% duty cycle) on a pin
 void tone(uint8_t pinNumber, unsigned int frequency, unsigned long duration = 0){
-    Hardware.gpio->tone(pinNumber, frequency, duration);
-    
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->tone(pinNumber, frequency, duration);
+    }
 }
 
 // Stops the generation of a square wave triggered by tone() 
 void noTone(uint8_t pinNumber){
-    Hardware.gpio->noTone(pinNumber);    
+    if (Hardware.gpio != nullptr) {
+        Hardware.gpio->noTone(pinNumber);
+    }
 }
     
 /// Reads a pulse (either HIGH or LOW) on a pin
 unsigned long pulseIn(uint8_t pinNumber, uint8_t state, unsigned long timeout = 1000000L){
-    return Hardware.gpio->pulseIn(pinNumber, state, timeout);    
-    
+    if (Hardware.gpio != nullptr) {
+        return Hardware.gpio->pulseIn(pinNumber, state, timeout);
+    } else {
+        return 0;
+    }
 }
     
 /// Reads a pulse (either HIGH or LOW) on a pin
 unsigned long pulseInLong(uint8_t pinNumber, uint8_t state, unsigned long timeout = 1000000L){
-    return Hardware.gpio->pulseInLong(pinNumber, state, timeout);        
+    if (Hardware.gpio != nullptr) {
+        return Hardware.gpio->pulseInLong(pinNumber, state, timeout);
+    } else {
+        return 0;
+    }
 }
     
     
