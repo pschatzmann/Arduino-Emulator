5265fb45d1b3988be2b8d060790dbd4b56a2ff72
pschatzmann <phil.schatzmann@gmail.com>
2025-09-29 17:01:29 +0200

    blink: add println

--- examples/rasperry_pi/blink/blink.ino
+++ examples/rasperry_pi/blink/blink.ino
@@ -5,8 +5,10 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Setting pin 13 HIGH");
   digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
   delay(1000);                     // Wait for 1 second
+  Serial.println("Setting pin 13 LOW");
   digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off
   delay(1000);                     // Wait for 1 second
 }
