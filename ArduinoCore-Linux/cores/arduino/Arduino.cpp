a83e7125e3d0b1dd7e6fe5179ab4deac6b613495
Serge Ageyev <justboss777@gmail.com>
2025-10-02 00:39:45 +0300

    Added missing functions bodies (random,randomSeed) for MSVC

--- ArduinoCore-Linux/cores/arduino/Arduino.cpp
+++ ArduinoCore-Linux/cores/arduino/Arduino.cpp
@@ -148,3 +148,34 @@ void yield(){
 }
 
 
+#if defined(_MSC_VER)
+
+// For some reason bodies is not available under MSVC
+
+#include <cstdlib>
+
+void randomSeed(unsigned long seed)
+{
+  if (seed != 0) {
+    srand(seed);
+  }
+}
+
+long random(long howbig)
+{
+  if (howbig == 0) {
+    return 0;
+  }
+  return rand() % howbig;
+}
+
+long random(long howsmall, long howbig)
+{
+  if (howsmall >= howbig) {
+    return howsmall;
+  }
+  long diff = howbig - howsmall;
+  return random(diff) + howsmall;
+}
+
+#endif
