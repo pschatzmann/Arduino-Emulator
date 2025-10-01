f106b8f460c3169a8a02f0ce64f9c0b73297920c
Serge Ageyev <justboss777@gmail.com>
2025-10-02 00:42:01 +0300

    Comments with notes on how to build under Windows with MSVC

--- ArduinoCore-Linux/cores/arduino/Arduino.cpp
+++ ArduinoCore-Linux/cores/arduino/Arduino.cpp
@@ -179,3 +179,51 @@ long random(long howsmall, long howbig)
 }
 
 #endif
+
+/*
+Notes on how to build under Windows with MSVC
+=============================================
+Under windows there is well known conflict between Arduino String.h and system string.h.
+If you read this, you probably seen a lot of problems with functiosn like strlen, strcmp, strcpy, memcpy etc.
+
+BACKROUND:
+Main problem reason is that windows file system is case insensitive and when some module trying to include 
+<string.h> or <String.h> will include file that is found first on include path.
+
+SOLUTION:
+To partially solve this problem, system <string.h> always included in angle brackets, while Arduino "String.h" in quotes with correct path.
+This way IF (and only if) system include paths are BEFORE Arduino include paths, THEN <string.h> will always include system file.
+Note: While you do not need to include String.h directly, it is included by Arduino.h, but you still have to setup path correctly.
+
+IMPLEMENTATION: HOW TO ENSURE THAT SYSTEM PATHS ARE BEFORE ARDUINO PATHS:
+You have to ensure that system include paths are before any Arduino include paths.
+On systems like VSCODE+MSVC system include paths are set via INCLUDE env var, while project-related path are in command line.
+Unfortunately %INCLUDE% have lower priority than path in command line (and /I compiler option accept only one path),
+you have to do some tricks to achive the goal.
+
+For VSCODE, you have to set paths in two places:
+1. .vscode/c_cpp_properties.json 
+Here are paths for IntelliSense and (code completion) you have to add ${env:INCLUDE} as first entry in "includePath" array
+"includePath": [ "${env:INCLUDE}",
+2. .vscode/tasks.json
+Here are paths for build (compilation), but since adding /I with multiple paths does not work,
+you cannot add /I${env:INCLUDE} as first entry in "args" array
+2.1 Option A: add add VS system paths one by one as separate /I entries BEFORE any Arduino related paths
+"/I${env:VCINSTALLDIR}Tools\\MSVC\\${env:VCToolsVersion}\\ATLMFC\\include",
+"/I${env:VCINSTALLDIR}Tools\\MSVC\\${env:VCToolsVersion}\\include",
+"/I${env:NETFXSDKDir}include\\um",
+"/I${env:WindowsSdkDir}include\\${env:UCRTVersion}\\ucrt",
+"/I${env:WindowsSdkDir}include\\${env:UCRTVersion}\\shared",
+"/I${env:WindowsSdkDir}include\\${env:UCRTVersion}\\um",
+"/I${env:WindowsSdkDir}include\\${env:UCRTVersion}\\winrt",
+"/I${env:WindowsSdkDir}include\\${env:UCRTVersion}\\cppwinrt",
+2.2 Option B: appens your project Arduino-related path to INCLUDE env var after system paths
+You have to add LIB, LIBPATH among with INCLUDE env vars to "env" section of "options" in tasks.json
+Example:
+"options": {
+...
+"env": { "LIB": "${env:LIB}", "LIBPATH": "${env:LIBPATH}",
+    "INCLUDE": "${env:INCLUDE};${workspaceFolder};${workspaceFolder}/tests;${workspaceFolder}/tests/sim;${workspaceFolder}/tests/sim/Arduino-Emulator/ArduinoCore-API/api;${workspaceFolder}/tests/sim/Arduino-Emulator/ArduinoCore-API/api/deprecated-avr-comp;${workspaceFolder}/tests/sim/Arduino-Emulator/ArduinoCore-Linux/cores/arduino",
+}
+2.3 Option C: invent something more elegant by yourself :)
+*/
