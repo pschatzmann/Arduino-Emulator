a85ee7ecc0080425c3085c976d8c1a6397179a3e
pschatzmann <phil.schatzmann@gmail.com>
2025-10-02 03:49:35 +0200

    WMath.cpp

---
+++ ArduinoCore-Linux/cores/arduino/WMath.cpp
@@ -0,0 +1,26 @@
+// WMath.cpp
+#include "Common.h"
+extern "C" {
+  #include <stdlib.h>
+}
+
+long random(long howbig) {
+  if (howbig == 0) {
+    return 0;
+  }
+  return (long)(rand() % howbig);
+}
+
+long random(long howsmall, long howbig) {
+  if (howsmall >= howbig) {
+    return howsmall;
+  }
+  long diff = howbig - howsmall;
+  return random(diff) + howsmall;
+}
+
+void randomSeed(unsigned long seed) {
+  if (seed != 0) {
+    srand(seed);
+  }
+}
