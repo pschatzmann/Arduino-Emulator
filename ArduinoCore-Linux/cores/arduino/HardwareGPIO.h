a652c9f2fa24f13e8fc120836d8c749c335086cc
pschatzmann <phil.schatzmann@gmail.com>
2025-10-03 19:41:55 +0200

    Doxygen

--- ArduinoCore-Linux/cores/arduino/HardwareGPIO.h
+++ ArduinoCore-Linux/cores/arduino/HardwareGPIO.h
@@ -5,22 +5,107 @@
 namespace arduino {
 
 /**
- * Abstract base class for GPIO functions
- **/
+ * @brief Abstract base class for GPIO (General Purpose Input/Output) functions
+ *
+ * HardwareGPIO defines the interface for hardware abstraction of GPIO operations
+ * across different platforms and microcontrollers. This class provides a unified
+ * API for digital and analog I/O operations that can be implemented by platform-specific
+ * classes.
+ *
+ * The class supports:
+ * - Digital pin operations (pinMode, digitalWrite, digitalRead)
+ * - Analog operations (analogRead, analogWrite, analogReference)
+ * - PWM and tone generation (analogWrite, tone, noTone)
+ * - Pulse measurement (pulseIn, pulseInLong)
+ *
+ * Platform-specific implementations should inherit from this class and provide
+ * concrete implementations for all pure virtual methods. Examples include
+ * HardwareGPIO_RPI for Raspberry Pi, or similar classes for other platforms.
+ *
+ * @note All methods in this class are pure virtual and must be implemented
+ *       by derived classes to provide platform-specific functionality.
+ *
+ * @see HardwareGPIO_RPI
+ * @see PinMode
+ * @see PinStatus
+ *
+ */
 class HardwareGPIO {
  public:
   HardwareGPIO() = default;
   virtual ~HardwareGPIO() = default;
 
+  /**
+   * @brief Configure the specified pin to behave as an input or output
+   * @param pinNumber The pin number to configure
+   * @param pinMode The mode to set (INPUT, OUTPUT, INPUT_PULLUP, etc.)
+   */
   virtual void pinMode(pin_size_t pinNumber, PinMode pinMode) = 0;
+
+  /**
+   * @brief Write a HIGH or LOW value to a digital pin
+   * @param pinNumber The pin number to write to
+   * @param status The value to write (HIGH or LOW)
+   */
   virtual void digitalWrite(pin_size_t pinNumber, PinStatus status) = 0;
+
+  /**
+   * @brief Read the value from a specified digital pin
+   * @param pinNumber The pin number to read from
+   * @return HIGH or LOW
+   */
   virtual PinStatus digitalRead(pin_size_t pinNumber) = 0;
+
+  /**
+   * @brief Read the value from the specified analog pin
+   * @param pinNumber The analog pin to read from (A0, A1, etc.)
+   * @return The analog reading on the pin (0-1023 for 10-bit ADC)
+   */
   virtual int analogRead(pin_size_t pinNumber) = 0;
+
+  /**
+   * @brief Configure the reference voltage used for analog input
+   * @param mode The reference type (DEFAULT, INTERNAL, EXTERNAL, etc.)
+   */
   virtual void analogReference(uint8_t mode) = 0;
+
+  /**
+   * @brief Write an analog value (PWM wave) to a pin
+   * @param pinNumber The pin to write to
+   * @param value The duty cycle (0-255 for 8-bit PWM)
+   */
   virtual void analogWrite(pin_size_t pinNumber, int value) = 0;
+
+  /**
+   * @brief Generate a square wave of the specified frequency on a pin
+   * @param _pin The pin on which to generate the tone
+   * @param frequency The frequency of the tone in hertz
+   * @param duration The duration of the tone in milliseconds (0 = continuous)
+   */
   virtual void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0) = 0;
+
+  /**
+   * @brief Stop the generation of a square wave triggered by tone()
+   * @param _pin The pin on which to stop generating the tone
+   */
   virtual void noTone(uint8_t _pin) = 0;
+
+  /**
+   * @brief Read a pulse (HIGH or LOW) on a pin
+   * @param pin The pin on which you want to read the pulse
+   * @param state Type of pulse to read (HIGH or LOW)
+   * @param timeout Timeout in microseconds (default 1 second)
+   * @return The length of the pulse in microseconds, or 0 if timeout
+   */
   virtual unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L) = 0;
+
+  /**
+   * @brief Alternative to pulseIn() which is better at handling long pulses
+   * @param pin The pin on which you want to read the pulse
+   * @param state Type of pulse to read (HIGH or LOW)
+   * @param timeout Timeout in microseconds (default 1 second)
+   * @return The length of the pulse in microseconds, or 0 if timeout
+   */
   virtual unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L) = 0;
 };
 
--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['digitalread_16',['digitalRead',['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#abaf2b8e0d8f46d059b850d4df5b0a863',1,'arduino::HardwareGPIO_RPI']]],
-  ['digitalwrite_17',['digitalWrite',['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#afb392ff358590dea32dd1d3e2198efd1',1,'arduino::HardwareGPIO_RPI']]],
+  ['digitalread_16',['digitalRead',['../classarduino_1_1_g_p_i_o_wrapper.html#a38c123198ff08bafe60d37050baa574e',1,'arduino::GPIOWrapper::digitalRead()'],['../classarduino_1_1_hardware_g_p_i_o.html#a408e951f42d9f765cd89c39fe84c8886',1,'arduino::HardwareGPIO::digitalRead()'],['../classarduino_1_1_remote_g_p_i_o.html#ad69e6552972f07584fa2c4c1d232badc',1,'arduino::RemoteGPIO::digitalRead()'],['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#abaf2b8e0d8f46d059b850d4df5b0a863',1,'arduino::HardwareGPIO_RPI::digitalRead()']]],
+  ['digitalwrite_17',['digitalWrite',['../classarduino_1_1_g_p_i_o_wrapper.html#a01ef49d52dfec84e3fa6375dd79da90d',1,'arduino::GPIOWrapper::digitalWrite()'],['../classarduino_1_1_hardware_g_p_i_o.html#af3608f342505f3885292b318a79bb587',1,'arduino::HardwareGPIO::digitalWrite()'],['../classarduino_1_1_remote_g_p_i_o.html#a6a05b7f35716a1981e732121d1e1d26e',1,'arduino::RemoteGPIO::digitalWrite()'],['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#afb392ff358590dea32dd1d3e2198efd1',1,'arduino::HardwareGPIO_RPI::digitalWrite()']]],
   ['dmabuffer_18',['DMABuffer',['../classarduino_1_1_d_m_a_buffer.html',1,'arduino']]],
   ['dmapool_19',['DMAPool',['../classarduino_1_1_d_m_a_pool.html',1,'arduino']]],
   ['dmapool_3c_20t_2c_20_5f_5fcache_5fline_5fsize_5f_5f_20_3e_20',['DMAPool&lt; T, __CACHE_LINE_SIZE__ &gt;',['../classarduino_1_1_d_m_a_pool.html',1,'arduino']]],
