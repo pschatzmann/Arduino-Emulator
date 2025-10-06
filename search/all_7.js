88929ed1ef4cb8f03a32eed117eff19789dcff7d
pschatzmann <phil.schatzmann@gmail.com>
2025-10-06 07:50:32 +0000

    Deploying to doxygen from @ pschatzmann/Arduino-Emulator@357c5ddc280ca039cba94d3f4a7bc044416891cb 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -3,8 +3,9 @@ var searchData=
   ['getbitposition_0',['getBitPosition',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#a399c4a2ffd3b8d5cf6230f8ff37bfd8c',1,'arduino::HardwareGPIO_FTDI']]],
   ['getchannel_1',['getChannel',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#abb4bcfebfcc349ba1ccf2cc2596e9c32',1,'arduino::HardwareGPIO_FTDI']]],
   ['getmpssecommand_2',['getMPSSECommand',['../classarduino_1_1_hardware_s_p_i___f_t_d_i.html#a002325b497ddfb8670076706a85150fa',1,'arduino::HardwareSPI_FTDI']]],
-  ['gpio_3',['GPIO',['../namespacearduino.html#a67230408a4be8e454f3947313e30c0e1',1,'arduino']]],
-  ['gpio_20spi_20i2c_4',['GPIO/SPI/I2C',['../index.html#autotoc_md2',1,'']]],
-  ['gpiosource_5',['GPIOSource',['../classarduino_1_1_g_p_i_o_source.html',1,'arduino']]],
-  ['gpiowrapper_6',['GPIOWrapper',['../classarduino_1_1_g_p_i_o_wrapper.html',1,'arduino']]]
+  ['getpwmstatistics_3',['getPWMStatistics',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#a1406a34d57feee1ff6a3237e0e04af27',1,'arduino::HardwareGPIO_FTDI']]],
+  ['gpio_4',['GPIO',['../namespacearduino.html#a67230408a4be8e454f3947313e30c0e1',1,'arduino']]],
+  ['gpio_20spi_20i2c_5',['GPIO/SPI/I2C',['../index.html#autotoc_md2',1,'']]],
+  ['gpiosource_6',['GPIOSource',['../classarduino_1_1_g_p_i_o_source.html',1,'arduino']]],
+  ['gpiowrapper_7',['GPIOWrapper',['../classarduino_1_1_g_p_i_o_wrapper.html',1,'arduino']]]
 ];
