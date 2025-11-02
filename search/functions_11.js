7bd93062b1e8715ba88e0f4679d0c4e2062ade96
pschatzmann <phil.schatzmann@gmail.com>
2025-11-02 19:56:52 +0000

    Deploying to doxygen from @ pschatzmann/Arduino-Emulator@fe14775ce65e5ca7c5ca7dd42bb5fc86a8749dbc 🚀

--- search/functions_11.js
+++ search/functions_11.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['_7ehardwaregpio_5fftdi_0',['~HardwareGPIO_FTDI',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#a5f2aae5a652c0fec089d9c6a92f02667',1,'arduino::HardwareGPIO_FTDI']]],
-  ['_7ehardwaregpio_5frpi_1',['~HardwareGPIO_RPI',['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#a79579e723138b5f682269b99d60b6fdd',1,'arduino::HardwareGPIO_RPI']]],
-  ['_7ehardwarei2c_5fftdi_2',['~HardwareI2C_FTDI',['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a850e6fac4e02d4b6a4746b38a7caa8b0',1,'arduino::HardwareI2C_FTDI']]],
-  ['_7ehardwaresetupftdi_3',['~HardwareSetupFTDI',['../classarduino_1_1_hardware_setup_f_t_d_i.html#abe3e0bad04dfd5425b5c3bdff8a589b5',1,'arduino::HardwareSetupFTDI']]],
-  ['_7ehardwaresetuprpi_4',['~HardwareSetupRPI',['../classarduino_1_1_hardware_setup_r_p_i.html#a3b162b1537ba1028d925a4d30a9f06e0',1,'arduino::HardwareSetupRPI']]],
-  ['_7ehardwarespi_5fftdi_5',['~HardwareSPI_FTDI',['../classarduino_1_1_hardware_s_p_i___f_t_d_i.html#ae5185a3e003b58d98287d26ed648959f',1,'arduino::HardwareSPI_FTDI']]],
-  ['_7eserialib_6',['~serialib',['../classserialib.html#ac44215001ae198f2c196bb7993327a4b',1,'serialib']]]
+  ['_7ehardwaregpio_5ffirma_0',['~HardwareGPIO_FIRMA',['../classarduino_1_1_hardware_g_p_i_o___f_i_r_m_a_t_a.html#a135d8c07d623570db55881fd8d2f989d',1,'arduino::HardwareGPIO_FIRMATA']]],
+  ['_7ehardwaregpio_5fftdi_1',['~HardwareGPIO_FTDI',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#a5f2aae5a652c0fec089d9c6a92f02667',1,'arduino::HardwareGPIO_FTDI']]],
+  ['_7ehardwaregpio_5frpi_2',['~HardwareGPIO_RPI',['../classarduino_1_1_hardware_g_p_i_o___r_p_i.html#a79579e723138b5f682269b99d60b6fdd',1,'arduino::HardwareGPIO_RPI']]],
+  ['_7ehardwarei2c_5fftdi_3',['~HardwareI2C_FTDI',['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a850e6fac4e02d4b6a4746b38a7caa8b0',1,'arduino::HardwareI2C_FTDI']]],
+  ['_7ehardwaresetupftdi_4',['~HardwareSetupFTDI',['../classarduino_1_1_hardware_setup_f_t_d_i.html#abe3e0bad04dfd5425b5c3bdff8a589b5',1,'arduino::HardwareSetupFTDI']]],
+  ['_7ehardwaresetuprpi_5',['~HardwareSetupRPI',['../classarduino_1_1_hardware_setup_r_p_i.html#a3b162b1537ba1028d925a4d30a9f06e0',1,'arduino::HardwareSetupRPI']]],
+  ['_7ehardwarespi_5fftdi_6',['~HardwareSPI_FTDI',['../classarduino_1_1_hardware_s_p_i___f_t_d_i.html#ae5185a3e003b58d98287d26ed648959f',1,'arduino::HardwareSPI_FTDI']]],
+  ['_7eserialib_7',['~serialib',['../classserialib.html#ac44215001ae198f2c196bb7993327a4b',1,'serialib']]]
 ];
