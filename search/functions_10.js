b0e966d3a796de545a87a40de82639570b267091
pschatzmann <phil.schatzmann@gmail.com>
2026-04-18 15:54:36 +0000

    Deploying to doxygen from @ pschatzmann/Arduino-Emulator@188a4184d5ec39fe53f8f1837bd8c8c6ee163bcb 🚀

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['write_0',['write',['../classarduino_1_1_hardware_c_a_n.html#ac22cd733cb28a1408e79e98667d23b57',1,'arduino::HardwareCAN::write()'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#abd37b42ecb72af90aabd162c7164fdba',1,'arduino::HardwareI2C_FTDI::write(uint8_t data) override'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#af32e14ec1472c43f8b42d79bff550a64',1,'arduino::HardwareI2C_FTDI::write(const uint8_t *data, size_t len) override'],['../classarduino_1_1_servo.html#a97038548529733aabd2d346d49349ac6',1,'arduino::Servo::write()']]],
+  ['write_0',['write',['../classarduino_1_1_hardware_c_a_n.html#ac22cd733cb28a1408e79e98667d23b57',1,'arduino::HardwareCAN::write()'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#abd37b42ecb72af90aabd162c7164fdba',1,'arduino::HardwareI2C_FTDI::write(uint8_t data) override'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#af32e14ec1472c43f8b42d79bff550a64',1,'arduino::HardwareI2C_FTDI::write(const uint8_t *data, size_t len) override'],['../classarduino_1_1_servo.html#a97038548529733aabd2d346d49349ac6',1,'arduino::Servo::write(int value)'],['../classarduino_1_1_servo.html#a97038548529733aabd2d346d49349ac6',1,'arduino::Servo::write(int value)']]],
   ['writebytes_1',['writeBytes',['../classserialib.html#aa14196b6f422584bf5eebc4ddb71d483',1,'serialib']]],
   ['writechar_2',['writeChar',['../classserialib.html#aa6d231cb99664a613bcb503830f73497',1,'serialib']]],
-  ['writemicroseconds_3',['writeMicroseconds',['../classarduino_1_1_servo.html#ad141b6f730f47e7c5d8a68a0ae02c32c',1,'arduino::Servo']]],
+  ['writemicroseconds_3',['writemicroseconds',['../classarduino_1_1_servo.html#ad141b6f730f47e7c5d8a68a0ae02c32c',1,'arduino::Servo::writeMicroseconds(int us)'],['../classarduino_1_1_servo.html#ad141b6f730f47e7c5d8a68a0ae02c32c',1,'arduino::Servo::writeMicroseconds(int us)']]],
   ['writestring_4',['writeString',['../classserialib.html#a6a32655c718b998e5b63d8cdc483ac6d',1,'serialib']]]
 ];
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['read_0',['read',['../classarduino_1_1_hardware_c_a_n.html#a1b4e9c0eec8f32a3ab4402234981d25b',1,'arduino::HardwareCAN::read()'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a9586c9041e8299857732b488c03631ba',1,'arduino::HardwareI2C_FTDI::read()'],['../classarduino_1_1_servo.html#a3cad11cdb66b3a635d329ed8ba5af235',1,'arduino::Servo::read()']]],
+  ['read_0',['read',['../classarduino_1_1_hardware_c_a_n.html#a1b4e9c0eec8f32a3ab4402234981d25b',1,'arduino::HardwareCAN::read()'],['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a9586c9041e8299857732b488c03631ba',1,'arduino::HardwareI2C_FTDI::read()'],['../classarduino_1_1_servo.html#a3cad11cdb66b3a635d329ed8ba5af235',1,'arduino::Servo::read() const'],['../classarduino_1_1_servo.html#a3cad11cdb66b3a635d329ed8ba5af235',1,'arduino::Servo::read() const']]],
   ['readbytes_1',['readBytes',['../classserialib.html#ab05e51ff3bc47c02d7d000d58b45a961',1,'serialib']]],
   ['readchar_2',['readChar',['../classserialib.html#a6c78b8a11ae7b8af57eea3dbc7fa237b',1,'serialib']]],
   ['readgpiostate_3',['readGPIOState',['../classarduino_1_1_hardware_g_p_i_o___f_t_d_i.html#ac53208736a6cff78871e15ab308b6e92',1,'arduino::HardwareGPIO_FTDI']]],
-  ['readmicroseconds_4',['readMicroseconds',['../classarduino_1_1_servo.html#a318e0759a932d2d879e8cb6b471689f9',1,'arduino::Servo']]],
+  ['readmicroseconds_4',['readmicroseconds',['../classarduino_1_1_servo.html#a318e0759a932d2d879e8cb6b471689f9',1,'arduino::Servo::readMicroseconds() const'],['../classarduino_1_1_servo.html#a318e0759a932d2d879e8cb6b471689f9',1,'arduino::Servo::readMicroseconds() const']]],
   ['readstring_5',['readString',['../classserialib.html#ab155c84352ddefe1304d391c19497ac1',1,'serialib']]],
   ['receivebytesendack_6',['receiveByteSendAck',['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a410b74ade94f8cb1566c4727fadc159c',1,'arduino::HardwareI2C_FTDI']]],
   ['receivedata_7',['receivedata',['../classarduino_1_1_hardware_i2_c___f_t_d_i.html#a330f77304015bd9a24428a49bb2f0945',1,'arduino::HardwareI2C_FTDI::receiveData()'],['../classarduino_1_1_hardware_s_p_i___f_t_d_i.html#abd25a1930b78c1c7ca4b274f1af3b12c',1,'arduino::HardwareSPI_FTDI::receiveData()']]],
